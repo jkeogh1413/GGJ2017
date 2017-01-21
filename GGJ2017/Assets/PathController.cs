2cb0edbd05c7bc7eb06b1ee937123d692d369874
Jesse Keogh <jkeogh1413@gmail.com>
2017-01-20 23:26:40 -0600

    Proof of concept for spring trap and waypoint

---
+++ GGJ2017/Assets/PathController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathController : MonoBehaviour {
+
+	Transform enemies;
+	Transform waypoints;
+
+	int curWaypoint = 1;
+	float walkSpeed = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+		enemies = transform.FindChild ("Enemies");
+		waypoints = transform.FindChild ("Waypoints");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Transform curWaypointTransform = waypoints.FindChild ("Waypoint" + curWaypoint.ToString ());
+		foreach (Transform enemy in enemies) {
+			//enemy.position = Vector3.Lerp (enemy.position, curWaypointTransform.position, 0.5f * Time.deltaTime);
+			enemy.LookAt(curWaypointTransform);
+			enemy.Translate(Vector3.forward * walkSpeed * Time.deltaTime);
+		}
+	}
+}
---
+++ GGJ2017/Assets/TriggerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerController : MonoBehaviour {
+
+	public float triggerForce = 1000f;
+	public Rigidbody triggerRB;
+
+	private bool forceTrapButtonPressed = false;
+
+	void Start() {
+		triggerRB = GameObject.Find("ForceTrap").GetComponent<Rigidbody> ();
+	}
+
+	void OnTriggerStay(Collider col) {
+		if (col.gameObject.tag == "Enemy" && Input.GetKeyDown(KeyCode.Space)) {
+			// do the thing
+			Debug.Log("doing the thing");
+			col.gameObject.GetComponent<Rigidbody> ().AddExplosionForce(triggerForce, new Vector3(triggerRB.transform.position.x, triggerRB.transform.position.y - 0.5f, triggerRB.transform.position.z), 10f);
+		}
+	}
+}
