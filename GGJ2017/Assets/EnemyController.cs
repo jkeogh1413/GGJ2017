c13cebb7ab18142e41b57b7279a56df3a5b1d99d
Jesse Keogh <jkeogh1413@gmail.com>
2017-01-21 10:52:57 -0600

    Basic pathing with waypoints working Don't forget to grandchild the Enemy after spawn

---
+++ GGJ2017/Assets/EnemyController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+	Transform waypoints;
+
+	int curWaypoint = 1;
+	float walkSpeed = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+		// Based on spawn, choose a path, and become child of Path.Enemies
+
+		waypoints = transform.parent.parent.FindChild ("Waypoints");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Transform curWaypointTransform = waypoints.FindChild ("Waypoint" + curWaypoint.ToString ());
+		transform.LookAt(curWaypointTransform);
+		transform.Translate(Vector3.forward * walkSpeed * Time.deltaTime);
+
+		if (Vector3.Distance (transform.position, curWaypointTransform.position) < 0.1f) {
+			if (curWaypoint < waypoints.childCount) {
+				curWaypoint++;
+			}
+		}
+	}
+}
