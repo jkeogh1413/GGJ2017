fffeba85c67df6154f30f2ec9144d3fa2f6f7f68
Jesse Keogh <jkeogh1413@gmail.com>
2017-01-21 16:21:56 -0600

    Button controllers for controlling traps

---
+++ GGJ2017/Assets/ButtonController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour {
+
+	public Transform[] traps;
+
+	void OnTriggerEnter(Collider col) {
+		if (col.tag == "Clicker") {
+			Debug.Log ("this time we are really doing it");
+			foreach (Transform trap in traps) {
+				trap.GetComponent<TriggerController> ().triggered = true;
+			}
+		}
+	}
+}
--- GGJ2017/Assets/TriggerController.cs
+++ GGJ2017/Assets/TriggerController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class TriggerController : MonoBehaviour {
 
 	public float triggerForce = 1000f;
+	public bool triggered = false;
 	public Rigidbody triggerRB;
 
 	private bool forceTrapButtonPressed = false;
@@ -12,8 +13,12 @@ public class TriggerController : MonoBehaviour {
 		triggerRB = gameObject.GetComponent<Rigidbody> ();
 	}
 
+	void LateUpdate() {
+		triggered = false;
+	}
+
 	void OnTriggerStay(Collider col) {
-		if (col.gameObject.tag == "Enemy" && Input.GetKeyDown(KeyCode.Space)) {
+		if (col.gameObject.tag == "Enemy" && (Input.GetKeyDown(KeyCode.Space) || triggered)) {
 			// do the thing
 			Debug.Log("doing the thing");
 			col.gameObject.GetComponent<Rigidbody> ().AddExplosionForce(triggerForce, new Vector3(triggerRB.transform.position.x, triggerRB.transform.position.y - 0.5f, triggerRB.transform.position.z), 10f);
