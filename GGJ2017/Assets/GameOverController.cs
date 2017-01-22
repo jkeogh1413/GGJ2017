690215e18feaef512153c4036c559279f9436c48
Jesse Keogh <jkeogh1413@gmail.com>
2017-01-21 22:25:57 -0600

    Game Over trigger

---
+++ GGJ2017/Assets/GameOverController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverController : MonoBehaviour {
+	void OnTriggerEnter(Collider col) {
+		if (col.tag == "Enemy") {
+			Debug.Log ("Game Over!");
+		}
+	}
+}
