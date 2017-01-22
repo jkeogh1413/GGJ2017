8658f7d6087d2e2a0445616fe2b2ae13f18c7e8a
Paul Martin <Dmart331@gmail.com>
2017-01-21 18:59:08 -0600

    Added trap door

---
+++ GGJ2017/Assets/Trigger2.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trigger2 : MonoBehaviour {
+
+	public float triggerForce = 1000f;
+	public bool triggered = false;
+	public Rigidbody triggerRB;
+
+	private bool forceTrapButtonPressed = false;
+
+	void Start() {
+		triggerRB = gameObject.GetComponent<Rigidbody> ();
+	}
+
+	void LateUpdate() {
+		triggered = false;
+	}
+
+	void OnTriggerStay(Collider col) {
+		if (col.gameObject.tag == "Enemy" && (Input.GetKeyDown(KeyCode.Space) || triggered)) {
+			// do the thing
+			Debug.Log("doing the thing");
+			col.gameObject.GetComponent<Collider> ().enabled = false;
+		}
+	}
+}
