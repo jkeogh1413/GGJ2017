c046ddeef47414324140e67bf93d649616d794eb
Jesse Keogh <jkeogh1413@gmail.com>
2017-01-21 18:17:12 -0600

    Button stuff sound hierarchy stuff

--- GGJ2017/Assets/EnemyController.cs
+++ GGJ2017/Assets/EnemyController.cs
@@ -3,11 +3,16 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour {
 	Transform waypoints;
+	Transform player;
 
 	int curWaypoint = 1;
 	float walkSpeed = 1.5f;
 	bool done = false;
     bool isWalking = false;
+
+	public string state = "neutral";
+	private bool hasBeenHappy = false;
+	private bool hasBeenSad = false;
 
     private RoundManager roundManager;
     private EnemyGetsWavedAt getsWavedAtBehavior;
@@ -17,6 +22,7 @@ public class EnemyController : MonoBehaviour {
 		// Based on spawn, choose a path, and become child of Path.Enemies
 
 		waypoints = transform.parent.parent.FindChild ("Waypoints");
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
         getsWavedAtBehavior = GetComponent<EnemyGetsWavedAt>();
         GameObject roundManagerGO = GameObject.Find("RoundManager");
         roundManager = roundManagerGO.GetComponent<RoundManager>();
@@ -26,15 +32,36 @@ public class EnemyController : MonoBehaviour {
 	void Update () {
         if (getsWavedAtBehavior.IsBeingWavedAt())
         {
-            // Do some thing
-        } else if (transform.position.y > 4f || transform.position.y < -1f) {
+			transform.LookAt (player);
+			if (!hasBeenHappy) {
+				state = "happy1";
+				//triggerSound (state);
+				hasBeenHappy = true;
+			} else if (hasBeenSad) {
+				state = "happy2";
+				//triggerSound (state);
+			}
+		} else if (transform.position.y > 4f) {
 			Destroy (gameObject, 5f);
+			//triggerSound ("flying");
 			done = true;
             roundManager.DudeDied();
+		} else if (transform.position.y < 1f) {
+			//triggerSound ("drowning");
+			done = true;
+			roundManager.DudeDied();
 		} else if (!done) {
 			Transform curWaypointTransform = waypoints.FindChild ("Waypoint" + curWaypoint.ToString ());
 			transform.LookAt (curWaypointTransform);
 			transform.Translate (Vector3.forward * walkSpeed * Time.deltaTime);
+
+			if (hasBeenHappy && hasBeenSad) {
+				state = "sad2";
+				//triggerSound (state);
+			} else if (hasBeenHappy) {
+				state = "sad1";
+				//triggerSound (state);
+			}
 
 			if (Vector3.Distance (transform.position, curWaypointTransform.position) < 0.1f) {
 				if (curWaypoint < waypoints.childCount) {
@@ -42,5 +69,11 @@ public class EnemyController : MonoBehaviour {
 				}
 			}
 		}
+	}
+
+	public void triggerSound(string category) {
+		Transform soundGroup = GameObject.Find ("EnemyAudio").transform.FindChild (category [0].ToString ().ToUpper () + category.Substring (1));
+		AudioSource audioSource = soundGroup.GetChild (Random.Range (0, soundGroup.childCount)).GetComponent<AudioSource> ();
+		audioSource.Play ();
 	}
 }
